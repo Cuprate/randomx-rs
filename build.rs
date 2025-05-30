1b3de8a18b9a4468498fc3bd0547cea2e17fa429
hinto.janai <hinto.janai@protonmail.com>
2025-05-30 14:24:16 -0400

    update

--- build.rs
+++ build.rs
@@ -20,7 +20,7 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use std::{env, fs::read_to_string};
+use std::env;
 
 use cmake::Config;
 
@@ -30,13 +30,8 @@ fn main() {
         .define("DARCH", "native")
         .build();
 
-    let lib_dir = if read_to_string("/etc/os-release").is_ok_and(|s| s.contains("Fedora")) {
-        "lib64"
-    } else {
-        "lib"
-    };
-
-    println!("cargo:rustc-link-search=native={}/{lib_dir}", randomx_path.display());
+    println!("cargo:rustc-link-search=native={}/lib64", randomx_path.display());
+    println!("cargo:rustc-link-search=native={}/lib", randomx_path.display());
     println!("cargo:rustc-link-lib=static=randomx");
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or("linux".to_string());
     let dylib_name = match target_os.as_str() {
